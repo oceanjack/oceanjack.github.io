813662d4668293168b59dfd6ab10dd8deefa4b76
oceanjack <oceanjack.app@gmail.com>
2014-12-20 22:25:16 +0800

    update

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,13 @@
-console.log('This would be the main JS file.');
+var playMusic = function() {
+  var play = false;
+  $('.head').on('click', function() {
+    if(play) {
+      $('audio')[0].pause();
+      $('.head').css('color','#999');
+    } else {
+      $('audio')[0].play();
+      $('.head').css('color','#fff');
+    }
+    play = !play;
+  }, false);
+}();
