9c0d118944b4df8dec0c2810b27acf52d2b30cb7
oceanjack <oceanjack.app@gmail.com>
2015-01-02 11:44:30 +0800

    update

--- javascripts/map.js
+++ javascripts/map.js
@@ -1,47 +1,47 @@
 var getMap = function() {
-	  var map = new BMap.Map("allmap");
-	  var point = new BMap.Point(118.855317, 32.035225);
-	  map.centerAndZoom(point, 5);
-	  map.enableScrollWheelZoom();
-	  var getPos = function(pos) {
-	    var points = [];
-	    for(var i = 0, l = pos.length; i < l; ++i) {
-	      points.push(new BMap.Point(pos[i].lng, pos[i].lat));
-	    }
-	    var len = points.length;
-	    var markers = [];
-	    for(var i = 0; i < len; ++i) {
-	      markers.push(new BMap.Marker(points[i]));
-	    }
-	    var markerClusterer = new BMapLib.MarkerClusterer(map, {markers:markers});
-	    markerClusterer.setMaxZoom(11);
-	    for(var i = 0; i < len; ++i) {
-	      markers[i].addEventListener("click", function(id) {
-	        return function() {
-	          var this_ = this;
-	          var getMsg = function(con) {
-	            var infoWindow = new BMap.InfoWindow(con);
-	            this_.openInfoWindow(infoWindow);
-	            window.setTimeout(function() {
-	            	infoWindow.redraw();
-	            }, 4700);
-	          };
-	        	var con = 
-              "<div class='article maparticle'>" +
-                "<div class='header'>" + pos[id].title + "</div>" +
-                "<div class='content'>" + 
-                  "<p>" + pos[id].text + "<p>" +
-                "<img src='" + pos[id].img + "'>"
-                "</div>" + 
-              "</div>";
-		        getMsg(con);
-	        };
-	      }(i));
-	    }
-	  };
+  var map = new BMap.Map("allmap");
+  var point = new BMap.Point(118.855317, 32.035225);
+  map.centerAndZoom(point, 5);
+  map.enableScrollWheelZoom();
+  var getPos = function(pos) {
+    var points = [];
+    for(var i = 0, l = pos.length; i < l; ++i) {
+      points.push(new BMap.Point(pos[i].lng, pos[i].lat));
+    }
+    var len = points.length;
+    var markers = [];
+    for(var i = 0; i < len; ++i) {
+      markers.push(new BMap.Marker(points[i]));
+    }
+    var markerClusterer = new BMapLib.MarkerClusterer(map, {markers:markers});
+    markerClusterer.setMaxZoom(11);
+    for(var i = 0; i < len; ++i) {
+      markers[i].addEventListener("click", function(id) {
+        return function() {
+          var this_ = this;
+          var getMsg = function(con) {
+            var infoWindow = new BMap.InfoWindow(con);
+            this_.openInfoWindow(infoWindow);
+            window.setTimeout(function() {
+              infoWindow.redraw();
+            }, 4700);
+          };
+          var con = 
+            "<div class='article maparticle'>" +
+              "<div class='header'>" + pos[id].title + "</div>" +
+              "<div class='content'>" + 
+                "<p>" + pos[id].text + "<p>" +
+              "<img src='" + pos[id].img + "'>"
+              "</div>" + 
+            "</div>";
+		      getMsg(con);
+	      };
+	    }(i));
+	  }
+	};
 
-    $.getJSON('/map.json', function(data) {
-      getPos(data);
-    });
-}();
-
+  $.getJSON('/map.json', function(data) {
+    getPos(data);
+  });
+};
+getMap();
