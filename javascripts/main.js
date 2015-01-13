584af51ea2849d06fcfab27327b5d222b66b166e
oceanjack <oceanjack.app@gmail.com>
2015-01-13 16:10:34 +0800

    update

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,14 +1,20 @@
 var playMusic = function() {
   var play = false;
-  $('.head').on('click', function() {
+  $('.head .name').on('click', function() {
     if(play) {
       $('audio')[0].pause();
-      $('.head').css('color','#999');
+      $('.head .name').css('color','#999');
     } else {
       $('audio')[0].play();
-      $('.head').css('color','#fff');
+      $('.head .name').css('color','#fff');
     }
     play = !play;
+  }, false);
+}();
+
+var historyBack = function() {
+  $('.head .back').on('click', function() {
+    history && history.back();
   }, false);
 }();
 
